8054255e3f4c601bd546337d14d8a11ed91ff0cd
LiuSirZ <liuwenbiao12138@163.com>
2021-01-22 11:30:37 +0800

    初始化模块

---
+++ rexxar_common/src/main/java/com/zherke/rexxar/common/constant/CommonConstant.java
@@ -0,0 +1,12 @@
+package com.zherke.rexxar.common.constant;
+
+/**
+ * 通用常量类
+ *      配置一些通用的无法分类的常量
+ * @author lwb
+ * @version 1.0.0
+ * @since 2021/1/21 17:42
+ */
+public class CommonConstant {
+
+}
---
+++ rexxar_common/src/main/java/com/zherke/rexxar/common/constant/RespTipConstant.java
@@ -0,0 +1,13 @@
+package com.zherke.rexxar.common.constant;
+
+/**
+ * 统一响应状态码常量
+ *
+ * @author lwb
+ * @version 1.0.0
+ * @since 2021/1/22 11:24
+ */
+public class RespTipConstant {
+    public static final String SUCCESS = "00";
+
+}
